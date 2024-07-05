b265c8e21475a2045b0410ab421d791d400562c9
Hans Jørgen Lachmann <hans@dynapro.no>
2024-07-05 14:06:08 +0200

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
+import ddLogo from '/src/assets/ddlogo.png'
+
 function Home() {
     return(<>
-        <h1>Welcome to Home!</h1>
-        <p>In remembrance of the great comedy film Dumb & Dumber from 1994 starring Jim Carrey and Jeff Daniels. All supply is deposited into ociswap liquidity pool. This is an experimental project of how to turn a DUMB shitcoin into something useful and valuable. Please check new website https://dumb.xrd soon...
+        <h1>Welcome to Dumb & Dumber Fan page!</h1>
+        <img src={ddLogo} alt="Dumb & Dumber" />
+        <p>In remembrance of the great comedy film Dumb & Dumber from 1994 starring Jim Carrey and Jeff Daniels. </p>
+        <p>All supply is deposited into ociswap liquidity pool.</p>
+        <p>This is an experimental project of how to turn a DUMB coin into something useful and valuable.
         Please connect your radix wallet to make a swap</p>
-        <img src="./assets/ddlogo.png" alt="Dumb & Dumber" />
+        
         </>
     )
 }
