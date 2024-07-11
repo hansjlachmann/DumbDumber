c0a8de5a19d210b47511d2533f0dd54092fcdd43
Hans Jørgen Lachmann <hans@dynapro.no>
2024-07-11 04:36:07 +0200

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import ddLogo from '/src/assets/ddlogo.png'
 
 function Home() {
     return(<>
-        <h1>Welcome to Dumb & Dumber Fan page!</h1>
+        <h1>Welcome to Dumb & Dumber Fan page!!</h1>
         <img src={ddLogo} alt="Dumb & Dumber" />
         <p>In remembrance of the great comedy film Dumb & Dumber from 1994 starring Jim Carrey and Jeff Daniels. </p>
         <p>All supply is deposited into ociswap liquidity pool.</p>
